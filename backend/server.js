0476a46360258727b714d247117978bdb1bc76bd
Coste Ionut <ionthekid@yahoo.com>
2025-01-08 16:50:01 +0000

    nodemon added to the server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000");
+app.listen(5800, () => {
+    console.log("Server started at http://localhost:5800");
 })
